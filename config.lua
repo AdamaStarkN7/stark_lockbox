c70292e2a2d784a84407ebeb4901e4a264a1a493
Adam Biegert <118407273+AdamaStarkN7@users.noreply.github.com>
2024-11-07 15:32:05 -0700

    Add files via upload

--- config.lua
+++ config.lua
@@ -2,16 +2,21 @@ Config = {}
 
 Config.debug = 'false' -- true or false
 
-Config.radial = 'qb' -- supported: 'qb' or 'ox'
+Config.radial = 'ox' -- supported: 'qb' or 'ox' -- default 'ox'
 
 Config.inventory = 'ox' -- supported: 'qb', 'ps', or 'ox'
 
 Config.lockboxSlots = 10 -- Recommended Value, Could Be Higher or Lower Depending On Server Needs
 
-Config.lockboxWeight = 120000 -- Recommended Value, Could Be Higher or Lower Depending On Server Needs 
+Config.lockboxWeight = 120000 -- Recommended Value, Could Be Higher or Lower Depending On Server Needs
 
 Config.leoJobs = {
     'police',
     'lssd',
-    -- add your server's police job here
+    -- add your server's police job here as found in qb-core/shared/jobs.lua
+}
+
+Config.leoJobTypes = {
+    'leo',
+    -- add your server's police job type here as found in qb-core/shared/jobs.lua
 }
